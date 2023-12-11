7051e4291d1020bc0595d73ca40bf3bfa4c21564
Muhammad Haris Abdullah <haris.abdullah@himmelz.com>
2023-12-11 15:56:40 +0500

    Implemented Netcode and Relay (Game Playable on multiple Devices)

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+public class PlayerNetwork : NetworkBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private Transform spawnedObject;
+    private NetworkVariable<int> networkVariable;
+    private Transform spawnedGameObject;
+    private void Awake()
+    {
+        networkVariable = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner) ;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        //networkVariable = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+     
+
+        // if(networkVariable.Value==1)
+        NetworkUIManager.instance.networkVariableText.text = networkVariable.Value.ToString();
+
+        networkVariable.OnValueChanged += (int previousValue, int newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; randomNumber: " + networkVariable.Value);
+            NetworkUIManager.instance.networkVariableText.text = networkVariable.Value.ToString();
+
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsOwner) return;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+             spawnedGameObject = Instantiate(spawnedObject);
+            spawnedGameObject.GetComponent<NetworkObject>().Spawn(true);
+            // networkVariable.Value = Random.Range(0, 100);
+            //TestClientRpc();
+        }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {           
+            spawnedGameObject.GetComponent<NetworkObject>().Despawn(true);
+        }
+
+        Vector3 moveDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+
+        Vector3 normalizedMoveDir = moveDir.normalized;
+
+        transform.position += normalizedMoveDir * moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc]
+    private void TestServerRpc()
+    {
+
+            networkVariable.Value = Random.Range(0, 100);    
+        Debug.Log(OwnerClientId +" ;TestServerRpc");
+    }
+
+    [ClientRpc]
+    private void TestClientRpc()
+    {
+        networkVariable.Value = Random.Range(0, 100);
+        Debug.Log(OwnerClientId + " ;TestServerRpc");
+    }
+}
---
+++ Assets/Scripts/TestingRelay.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using QFSW.QC;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+
+public class TestingRelay : MonoBehaviour
+{
+
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+
+            Debug.Log("Signed in +" + AuthenticationService.Instance.PlayerId);
+        };
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    [Command]
+    private async void CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartHost();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log("Relalay Exception : " + e);
+        }
+
+    }
+    [Command]
+    private async void JoinRelay(string joinCode)
+    {
+        try
+        {
+         JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log("Relalay Exception : " + e);
+        }
+
+    }
+}
