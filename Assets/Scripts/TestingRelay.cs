a6a27739ff4b640390f26dadcfd7da3afdda2e47
Muhammad Haris Abdullah <haris.abdullah@himmelz.com>
2023-12-12 19:27:49 +0500

    Implemented 2-way voice chat vivox

--- Assets/Scripts/TestingRelay.cs
+++ Assets/Scripts/TestingRelay.cs
@@ -10,10 +10,13 @@ using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
 using StarterAssets;
+using Unity.Services.Vivox;
+using VivoxUnity;
+using System;
 
 public class TestingRelay : NetworkBehaviour
 {
-
+    private ILoginSession LoginSession;
     private async void Start()
     {
         await UnityServices.InitializeAsync();
@@ -25,6 +28,7 @@ public class TestingRelay : NetworkBehaviour
         };
 
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        VivoxService.Instance.Initialize();
     }
 
     [Command]
@@ -70,4 +74,82 @@ public class TestingRelay : NetworkBehaviour
         }
 
     }
+
+
+    public void JoinChannel(string channelName, ChannelType channelType, bool connectAudio, bool connectText, bool transmissionSwitch = true, Channel3DProperties properties = null)
+    {
+        if (LoginSession.State == LoginState.LoggedIn)
+        {
+            Channel channel = new Channel(channelName, channelType, properties);
+
+            IChannelSession channelSession = LoginSession.GetChannelSession(channel);
+
+            channelSession.BeginConnect(connectAudio, connectText, transmissionSwitch, channelSession.GetConnectToken(), ar =>
+            {
+                try
+                {
+                    channelSession.EndConnect(ar);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"Could not connect to channel: {e.Message}");
+                    return;
+                }
+            });
+        }
+        else
+        {
+            Debug.LogError("Can't join a channel when not logged in.");
+        }
+    }
+
+    [Command]
+    public void Login(string displayName = null)
+    {
+        var account = new Account(displayName);
+        bool connectAudio = true;
+        bool connectText = true;
+
+        LoginSession = VivoxService.Instance.Client.GetLoginSession(account);
+        LoginSession.PropertyChanged += LoginSession_PropertyChanged;
+
+        LoginSession.BeginLogin(LoginSession.GetLoginToken(), SubscriptionMode.Accept, null, null, null, ar =>
+        {
+            try
+            {
+                LoginSession.EndLogin(ar);
+            }
+            catch (Exception e)
+            {
+                // Unbind any login session-related events you might be subscribed to.
+                // Handle error
+                return;
+            }
+            // At this point, we have successfully requested to login. 
+            // When you are able to join channels, LoginSession.State will be set to LoginState.LoggedIn.
+            // Reference LoginSession_PropertyChanged()
+        });
+    }
+
+    // For this example, we immediately join a channel after LoginState changes to LoginState.LoggedIn.
+    // In an actual game, when to join a channel will vary by implementation.
+    private void LoginSession_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        var loginSession = (ILoginSession)sender;
+        if (e.PropertyName == "State")
+        {
+            if (loginSession.State == LoginState.LoggedIn)
+            {
+                bool connectAudio = true;
+                bool connectText = true;
+
+                // This puts you into an echo channel where you can hear yourself speaking.
+                // If you can hear yourself, then everything is working and you are ready to integrate Vivox into your project.
+               // JoinChannel("TestChannel", ChannelType.Echo, connectAudio, connectText);
+                // To test with multiple users, try joining a non-positional channel.
+                 JoinChannel("MultipleUserTestChannel", ChannelType.NonPositional, connectAudio, connectText);
+            }
+        }
+    }
+
 }
